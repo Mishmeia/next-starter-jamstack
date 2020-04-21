cad088278f2d83938be98208ac68ee8804d61bfe
Mishmeia <iarlobruno@hotmail.com>
2020-04-20 21:22:31 -0300

    Update gabriella.js

--- pages/gabriella.js
+++ pages/gabriella.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import styled from 'styled-components'
 
+const TemaTeste = styled.section`
+  height: 100vh;
+  background: ${props => props.theme.bg};
+  color: ${props => props.theme.fontColor};
+`;
 const Gabriella = () => (
   <div>
     <Head>
       <title>Para Gabriella ❤️</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
+    <TemaTeste>
       <h1>Para Gabriella ❤️:</h1>
-
+    </TemaTeste>
     <style jsx>{`
       :global(html,body) {
         margin: 0;
@@ -37,9 +44,8 @@ const Gabriella = () => (
         color: pink;
         text-decoration: none;
       }
-
-      .content {
-        padding: 0 32px;
+      .topo {
+        height: 100vh;
       }
     `}</style>
   </div>
